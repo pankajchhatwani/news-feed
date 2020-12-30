fefed217904a363af2e8cb2e57bd7cb78e383fa9
Pankaj8460 <76781896+Pankaj8460@users.noreply.github.com>
2020-12-30 21:06:09 +0530

    Update NewsApi.php

--- Demo/News/Model/Api/NewsApi.php
+++ Demo/News/Model/Api/NewsApi.php
@@ -7,37 +7,26 @@ class NewsApi implements \Demo\News\Api\NewsInterface
     protected $_newsFactory;
 
     public function __construct(
+        \Demo\News\Model\ResourceModel\News $resource,
         \Demo\News\Model\NewsFactory $newsFactory
 
     ) {
+        $this->resource = $resource;
         $this->_newsFactory = $newsFactory;
     }
 
     public function getById($id)
     {
-        try {
-            $model = $this->_newsFactory->create()->getCollection()->addFieldToFilter('news_id',$id)->getFirstItem();
+        $model = $this->_newsFactory->create();
+        $this->resource->load($model, $id);
 
-            if (!$model->getNewsId()) {
-                throw new \Magento\Framework\Exception\LocalizedException(
-                    __('no data found')
-                );
-            }
-
-            return $model;
-        } catch (\Magento\Framework\Exception\LocalizedException $e) {
-            $returnArray['error'] = $e->getMessage();
-            $returnArray['status'] = 0;
-            $this->getJsonResponse(
-                $returnArray
-            );
-        } catch (\Exception $e) {
-            $this->createLog($e);
-            $returnArray['error'] = __('unable to process request');
-            $returnArray['status'] = 2;
-            $this->getJsonResponse(
-                $returnArray
+        if (!$model->getNewsId()) {
+            throw new \Magento\Framework\Exception\LocalizedException(
+                __('no data found')
             );
         }
+
+        return $model;
+
     }
 }
